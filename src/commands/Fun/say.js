c928d084a6a04df3578df78b345937e5d52fff00
justme0001 <86416808+justme0001@users.noreply.github.com>
2021-07-01 15:46:55 +0530

    Update say.js

--- src/commands/Fun/say.js
+++ src/commands/Fun/say.js
@@ -1,17 +1,17 @@
 module.exports = {
-    name: "say",
-    description: "Say anything, the bot will repeat it.",
-    category: "fun",
-    aliases: ["echo", "parrot"],
-    execute: (client, message, args) => {
-        
-        if (!args[0]) return message.reply('how can i say nothing?')
-        if (!await client.isBotOwner(message.author.id)) {
-            message.delete();
-            message.channel.send(args.join(" "))
-        } else {
-            message.delete();
-            message.channel.send(args.join(" ") + "\n-" + message.author.tag + "("+message.author.id + ")")
-        }
-    }
+  name: "say",
+  description: "Say anything, the bot will repeat it.",
+  category: "fun",
+  aliases: ["echo", "parrot"],
+  execute: async (client, message, args) => {
+      
+      if (!args[0]) return message.reply('how can i say nothing?')
+      if (await client.isBotOwner(message.author.id)) {
+          message.delete();
+          message.channel.send(args.join(" "))
+      } else {
+          message.delete();
+          message.channel.send(args.join(" ") + "\n-" + message.author.tag + "("+message.author.id + ")")
+      }
+  }
 }
