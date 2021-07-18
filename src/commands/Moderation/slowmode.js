fbc057a6e077b99e881b41ca3124fa727d1cafb7
justme <86416808+justme0001@users.noreply.github.com>
2021-07-18 18:17:40 +0530

    Update slowmode.js
    fixed `slowmode.js`

--- src/commands/Moderation/slowmode.js
+++ src/commands/Moderation/slowmode.js
@@ -8,15 +8,34 @@ module.exports = {
         if (message.guild.me.hasPermission["MANAGE_CHANNEL"]) return message.reply("i have no permission to do that")
         .then(m => {m.delete({timeout:5000})});
 
-        const slowmodeSEC = await args[1]
+        const MOD = message.author.id
+        const MOD1 = message.author.tag
 
-        message.channel.setRateLimitPerUser(slowmodeSEC, `Used by ${message.author.tag}(${message.author.id})`);
-        message.channel.send(`Set slowmode to ${slowmodeSEC}`)
+        const { channel } = message
 
-
-        if (!args.length) {
-            message.channel.send(`Removed slowmode`)
-            message.channel.setRateLimitPerUser(0, `Used by ${message.author.tag}(${message.author.id})`);
+        if (args.length < 1) {
+          message.reply('Please provide a duration')
+          return
         }
+    
+        let duration = args.shift().toLowerCase()
+        if (duration === 'off') {
+          duration = 0
+        }
+
+        if (duration > 21600){
+            message.reply("i can't keep slowmode upto 6 hours, LOL")
+            return
+        }
+    
+        if (isNaN(duration)) {
+          message.reply(
+            'Please provide either a number of seconds or the word "off"'
+          )
+          return
+        }
+      
+        channel.setRateLimitPerUser(duration, `Requested by ${MOD1}(${MOD})`)
+        message.reply(`The slowmode for this channel has been set to ${duration}`)
     }
 }
